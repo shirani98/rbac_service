92a6dabc0d95115169d70d7bf77dbb63597f47ee
Mahdi Shirani <shirani9882@gmail.com>
2024-01-23 14:17:28 +0330

    fix: update data

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='RBAC_Service',
-    version='0.1',
+    name="RBAC_service",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
         'Django',
     ],
     include_package_data=True,
-    license='MIT',
-    description='Django Role-Based Permission App',
-    long_description=open('README.md').read(),
-    url='https://github.com/yourusername/your-django-app',
-    author='Mahdi Shirani',
-    author_email='Shirani9882@gmail.com',
+    license="MIT",
+    description="Django Role-Based Permission App",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/shirani98/rbac_service",
+    author="Mahdi Shirani",
+    author_email="Shirani9882@gmail.com",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Framework :: Django',
