18efd07c29fddae79b137b6be52048f91d9fec0c
Mahdi Shirani <shirani9882@gmail.com>
2024-01-23 14:39:32 +0330

    fix: update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="RBAC_service",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     install_requires=[
         'Django',
